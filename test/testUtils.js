b693f3b0f798c1244b396c185405f786ffa40cc0
annaVih <anna-kobzii@mail.ru>
2019-02-16 13:41:55 +0000

    Write reusable test utils functions

---
+++ test/testUtils.js
@@ -0,0 +1,13 @@
+import checkPropTypes from 'check-prop-types';
+
+export const findByTestAttr = (wrapper, val) => 
+    wrapper.find(`[data-test="${val}"]`);
+
+export const checkProps = (component, confirmingProps) => {
+    const propError = checkPropTypes(component.propTypes,
+    confirmingProps,
+    'prop',
+    component.name
+    )
+    expect(propError).toBeUndefined();
+}
