fbdc51feb84a0af40c271439fd61cb38e96b0f85
annaVih <anna-kobzii@mail.ru>
2019-02-17 14:07:14 +0000

    Write test for guessWords component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import GuessWords from './GussedWords';
+import Congrats from './Congrats';
 
 class App extends Component {
   render() {
     return (
-      <div data-test="">
-
+      <div className="container">
+      <h1>Guess word gamer</h1>
+        <Congrats success={true}/>
+        <GuessWords guessedWords={[
+          {
+            guessedWord: 'train',
+            letterMatchCount: 3
+          }
+        ]}/>
       </div>
     );
   }
